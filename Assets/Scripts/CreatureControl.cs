84900d3466d9b508505dae9f0d39b047f923db69
Erik Uggeldahl <erikuggeldahl@gmail.com>
2014-04-26 01:56:44 -0400

    Initial commit. Contains initial, player, controls, lighting, and

---
+++ Assets/Scripts/CreatureControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreatureControl : MonoBehaviour
+{
+    Vector3 zMask = new Vector3(1f, 1f, 0f);
+
+    Camera mainCam;
+
+    float moveSpeed = 60f;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 mousePos = Vector3.Scale(mainCam.ScreenToWorldPoint(Input.mousePosition), zMask);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector2 toMouse = (mousePos - transform.position).normalized;
+
+            if (Vector2.Distance(mousePos, transform.position) > 0.6f)
+                rigidbody2D.AddForce(toMouse * moveSpeed);
+
+            transform.LookAt(mousePos);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = (Vector2)mousePos;
+
+            if (!particleSystem.isPlaying)
+                particleSystem.Play();
+        }
+
+        //Vector2 view = mainCam.ScreenToViewportPoint(Input.mousePosition);
+        //if (view.x < 0.1)
+        //    renderer.material.color = Color.red;
+    }
+}
