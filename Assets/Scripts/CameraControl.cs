cc0e6fa3c40215b5560b9cbf34506224741dc250
Erik Uggeldahl <erikuggeldahl@gmail.com>
2014-04-26 11:12:44 -0400

    Added camera control.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour
+{
+    [SerializeField]
+    Transform toFollow;
+
+    float followSpeed = 6f;
+
+    void Start()
+    {
+	
+    }
+
+    void Update()
+    {
+        var camPos = camera.WorldToViewportPoint(transform.position);
+        var toFollowPos = camera.WorldToViewportPoint(toFollow.position).ZMask();
+        var distance = Vector3.Distance(camPos, toFollowPos);
+
+        if (distance > 0.2)
+        {
+            var followMove = Vector3.MoveTowards(transform.position, toFollow.position, followSpeed * (1 + distance) * Time.deltaTime);
+            followMove.z = transform.position.z;
+            transform.position = followMove;
+        }
+    }
+}
+
